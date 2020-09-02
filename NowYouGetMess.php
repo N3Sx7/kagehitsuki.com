adc2eea44bb51515a48bf96e24c47fafb914d4c4
N3Sx7 <70651679+N3Sx7@users.noreply.github.com>
2020-09-03 00:29:45 +0900

    Added new files

---
+++ NowYouGetMess.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$nama = filter_input(INPUT_POST, 'YourHaUlder');
+$email = filter_input(INPUT_POST, 'EmHp');
+$alamat = filter_input(INPUT_POST, 'Almt');
+$umur = filter_input(INPUT_POST, 'oilod');
+
+if(!empty($nama)){
+
+}
+else{
+    echo "Bagian Nama Masih Kosong";
+    die();
+}
+
+>
